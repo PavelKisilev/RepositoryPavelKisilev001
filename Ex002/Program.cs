64c64660893a723408dbee9c696e82876e7b8975
PavelName <95@ya.ru>
2023-01-10 19:01:57 +0300

    new file:   .gitignore 	new file:   Ex001/Ex001.csproj 	new file:   Ex001/Program.cs 	new file:   Ex002/Ex002.csproj 	new file:   Ex002/Program.cs 	.vscode/

---
+++ Ex002/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число 1");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x == y*y) { Console.WriteLine("Первое число является квадратом второго");} 
+else {Console.WriteLine("Первое число не является квадратом второго"); }
