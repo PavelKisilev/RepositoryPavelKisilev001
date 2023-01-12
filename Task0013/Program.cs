eda34172627b80c83d844e6061234c8413540841
PavelName <95@ya.ru>
2023-01-12 21:57:58 +0300

    End

--- Task0013/Program.cs
+++ Task0013/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Обращаться к числу как к строке нельзя.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine()); // получили число
+int y=0;
+int z=x;
+Console.WriteLine("Запущен цикл");
+
+while (z != 0) // Цикл позволяет определить число разрядов. 
+    {
+        z=z/10;            // Цикл надо остановить, в момент равенства остатка нулю.
+        y=y+1;
+     } 
+Console.WriteLine("Остановлен цикл");
+
+if (y<3) {Console.Write("Третьей цифры нет");}// Если y<3 то третьей цифры нет, "y" - счётчик числа разрядов.
+else                                          // Иначе выводим третью цифру. 
+{ 
+    int x1=Convert.ToInt32(Math.Pow(10,(y-3)));
+    Console.WriteLine(x1);
+    x=x/x1;
+    x=x%10;
+    Console.Write(x);
+}
