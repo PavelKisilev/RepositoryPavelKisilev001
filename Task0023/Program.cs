4be834c86f269d0b7d9e41d6e6c2c00b2e189f03
PavelName <95@ya.ru>
2023-01-12 23:57:27 +0300

    End

--- Task0023/Program.cs
+++ Task0023/Program.cs
@@ -3,3 +3,13 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine("Введите число (N)");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = 1;
+int z = 1;
+while (y<=x)
+    {
+    z = Convert.ToInt32(Math.Pow(y,3));
+    Console.Write($"{z}, ");
+    y=y+1;
+    }
