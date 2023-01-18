dbcb1570f316eece59a03d174d2be3b3faec8d4f
PavelName <95@ya.ru>
2023-01-18 17:20:20 +0300

    End

---
+++ Task0041/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Начинайте вводить числа, по окончанию ввода введите End"); 
+string element = string.Empty;
+string end = "End";
+int i = 0;
+element = Console.ReadLine();    
+    
+    while (element != end ) 
+    {   if (Convert.ToInt32(element)>0) {i++;}
+    Console.WriteLine("Вводите следующие число");
+    element = Console.ReadLine();
+    } 
+        
+Console.WriteLine($"Число положительных чисел рано {i}");
