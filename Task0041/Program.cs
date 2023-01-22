92b619a4d8586999580e1cbabacb83ef7a70c4f8
PavelName <95@ya.ru>
2023-01-22 22:24:06 +0300

    00

--- Task0041/Program.cs
+++ Task0041/Program.cs
@@ -2,7 +2,7 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Начинайте вводить числа, по окончанию ввода введите End"); 
+Console.WriteLine("Начинайте вводить числа, по окончанию ввода введите End");                                                                                                                                          
 string element = string.Empty;
 string end = "End";
 int i = 0;
--- Task0043/Program.cs
+++ Task0043/Program.cs
@@ -1,20 +1,10 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5
 
-double[] InputMean = new double[4]; // Создаётся массив для заполнения входящими значениями двух прямых.
-{ Console.WriteLine($"Введите значение чисел b1, k1, b2 и k2 поочерёдно: "); }
-    for (int i = 0; i < InputMean.Length; i++)
-    {
-        InputMean[i] = Convert.ToDouble(Console.ReadLine());
-    }
-double x = 0;
-double y = 0;
-
-InputMeaning(InputMean[0], InputMean[1], InputMean[2], InputMean[3]);
-
-
-void InputMeaning(double b1, double b2, double k1, double k2)
+void InputMeaning(double [] InputMean)
 {
+    double x = 0;
+    double y = 0;
     x = (InputMean[2] - InputMean[0]) / (InputMean[1] - InputMean[3]);
     y = InputMean[1] * (x) + InputMean[0];
 
@@ -24,6 +14,19 @@ void InputMeaning(double b1, double b2, double k1, double k2)
         Console.WriteLine($"Точка пересечения ( {IntersectionPoint[0]} , {IntersectionPoint[1]} )");
     }
 }
+
+double[] InputMean = new double[4]; // Создаётся массив для заполнения входящими значениями двух прямых.
+{ Console.WriteLine($"Введите значение чисел b1, k1, b2 и k2 поочерёдно: "); }
+    for (int i = 0; i < InputMean.Length; i++)
+    {
+        InputMean[i] = Convert.ToDouble(Console.ReadLine());
+    }
+
+
+InputMeaning(InputMean);
+
+
+
 
 
 
