cae7a5476ccdc33d4526c034237ef9ac29f2c139
PavelName <95@ya.ru>
2023-01-26 15:51:24 +0300

    End

--- Task0060/Program.cs
+++ Task0060/Program.cs
@@ -5,7 +5,6 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-
 Console.WriteLine("Введите измерении массива i");
 int i = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите измерении массива k");
@@ -14,9 +13,9 @@ Console.WriteLine("Введите измерении массива j");
 int j = Convert.ToInt32(Console.ReadLine());
 const int CellWidth = 3;
 int[,,] Array = new int[i, k, j];
+
 FillArray(Array);
 PrintArray(Array);
-
 
 void FillArray(int[,,] Collection)
 {
@@ -25,13 +24,13 @@ void FillArray(int[,,] Collection)
         for (int k = 0; k < Collection.GetLength(1); k++)
         {
             for (int j = 0; j < Collection.GetLength(2); j++)
-            {
-                int temp = 0;
+            {                                 
+                int temp;
                 while (true)
                 {
-                    temp = new Random().Next(10, 19);
-                    if (Check(Collection, temp)) break;
-                    
+                    temp = new Random().Next(10, 99);
+                    if (Check(Collection, temp)) continue;
+                    else break;
                 }
                 Collection[i, k, j] = temp;
             }
@@ -47,7 +46,7 @@ bool Check(int[,,] Collection, int value)
         {
             for (int j= 0; j < Collection.GetLength(2); j++)
             {
-                if (value == Collection[i, k, j]) return true;
+                if (Collection[i, k, j] == value) return true;
             }
         }
     }
@@ -65,10 +64,13 @@ void PrintArray(int[,,] Collection)
             {
                 Console.Write($"{Collection[i, k, j],CellWidth}");
 
-                Console.Write($"({i}, {k}, {j}) ");
+                Console.Write($"({k}, {j}, {i}) ");
+
+                
             }
-        }
-        Console.WriteLine();
+            Console.WriteLine();
+        }   
+        
     }
 
 }
