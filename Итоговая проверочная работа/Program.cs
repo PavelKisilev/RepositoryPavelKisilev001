82055016aa12ac81bf72080d14807ee04ee5c5ec
PavelName <95@ya.ru>
2023-02-05 21:58:07 +0300

    InoutSymbolAndPrintArrayr

--- Итоговая проверочная работа/Program.cs
+++ Итоговая проверочная работа/Program.cs
@@ -1,20 +1,52 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 9
-// m = 3, n = 2 -> A(m,n) = 29
+﻿
+// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
 
 
-Console.WriteLine("Введите натуральное число m"); 
-int m = Convert.ToInt32(Console.ReadLine()); 
-Console.WriteLine("Введите натуральное число n"); 
-int n = Convert.ToInt32(Console.ReadLine()); 
+// Этапы выолненения работы:
+// 1. Сформировать массив данных посредством ввода с клавиатуры, превышающий 3 символа.
+// 2. преобразовать полученный массив, в удовлетворяющий условию.
+// 3. вывести полученный результат на экран.
+// Работы пыполнять с использованием логики проектирования приложений "Сверху-вниз".
 
-Console.WriteLine(Askerman(m,n));
 
 
-int Askerman(int m, int n)
+String[] Mass = new String[4];
+InputSymbol(Mass);
+//Change(Mass); // снижение колличества символов в массиве до 3-х и менее.
+PrintArray(Mass); // печать символов массива.
+
+
+void PrintArray(string[] Array)
 {
-    if (m == 0) return (n+1);
-    else if (m>0 && n==0) return Askerman(m-1,1);
-    else if (m>0 && n>0) return Askerman(m-1,Askerman(m,n-1));
-    else return (n+1);
+    string symbol = "";
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{symbol + Array[i]}, ");
+    }
 }
+
+
+void InputSymbol(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine("Введите символ или символы");
+        Array[i] = new(Console.ReadLine());
+        while (Array[i].Length <= 1)
+        {
+            Console.WriteLine("Введите дополнительный символ");
+            Array[i] = new(Array[i] + Console.ReadLine());
+        }
+
+    }
+
+}
+
+
